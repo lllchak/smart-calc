becf461224956dbc70a3399d0898ab46f915d043
lllchak <mediumchak@yandex.ru>
2023-01-03 12:13:10 +0300

    refactores files structure

--- src/backend/calculation/calculation.c
+++ src/backend/calculation/calculation.c
@@ -18,6 +18,21 @@ cflag calculate(token* postfix, size_t plength, double x, double* ans) {
 }
 
 double* gresults(token* postfix, size_t plength, double lx, double rx, double step) {
+    /*
+    Description:
+        Generates resulting points array
+    
+    Args:
+        (token*) postfix : RPN tokens
+        (size_t) plength : Length of RPN
+        (double) lx      : Left border
+        (double) rx      : Right border
+        (double) step    : Calculation step
+
+    Returns:
+        Array of points to draw graph with
+    */
+
     size_t respoints = ceil((double)abs((int)lx - (int)rx) / step);
     double* results = (double*)calloc(respoints, sizeof(double));
     double tans = 0.0;
--- src/backend/calculation/calculation.h
+++ src/backend/calculation/calculation.h
@@ -13,6 +13,16 @@
  */
 cflag calculate(token* postfix, size_t plength, double x, double* ans);
 
+/**
+ * @brief          Generates resulting points array
+ * 
+ * @param postfix  RPN tokens
+ * @param plength  Length of PRN
+ * @param lx       Left border
+ * @param rx       Right border
+ * @param step     Calculation step
+ * @return         Array of points to draw graph with
+ */
 double* gresults(token* postfix, size_t plength, double lx, double rx, double step);
 
 #endif  // SRC_CALCULATION_CALCULATION_H_
